d4601b32f6ff707da10ec12ea1bd38646c357471
Vanessa <vanessaalbertii01@gmail.com>
2026-07-02 05:17:09 -0300

    Eliminando test de google del codigo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { ForeignAidPage } from './pages/ForeignAidPage'
 import { AuthProvider } from './store/AuthContext'
-import { TestGoogle } from './components/TestGoogle'
 
 export default function App() {
   return (
     <AuthProvider>
-      <TestGoogle />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
